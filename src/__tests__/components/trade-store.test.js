2eee456bf8e9a5073807c296f1b7ad85b3f4f72e
Hemant Nigam <hemantni@ciena.com>
2022-09-01 09:10:56 +0530

    updated test case

--- src/__tests__/components/trade-store.test.js
+++ src/__tests__/components/trade-store.test.js
@@ -21,7 +21,7 @@ describe("Components/trade-store", () => {
     expect(linkElement).toBeInTheDocument();
   });
 
-  it("should renders trade list at initial render", () => {
+  it("should renders trade form when clicked on add button", () => {
     fireEvent.click(screen.getByTestId("add-trade-btn"));
     const formComponent = screen.getByTestId("trade-form");
     expect(formComponent).toBeInTheDocument();
